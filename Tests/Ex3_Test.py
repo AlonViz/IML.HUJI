0f9c5cf8d3a11529b6feeb48f85037c5b0d89353
alonv <alonvizner1@gmail.com>
2022-04-22 11:58:27 +0300

    Ex3: loss_functions update: 3.2.1

--- Tests/Ex3_Test.py
+++ Tests/Ex3_Test.py
@@ -1,9 +1,10 @@
 import numpy as np
 from typing import NoReturn
 from numpy.linalg import pinv
-from IMLearn.metrics import misclassification_error
+from IMLearn.metrics import misclassification_error, accuracy
 
-y_true = np.concatenate([np.ones(13), np.zeros(7)])
+y_true = np.concatenate([np.ones(7), np.zeros(13)])
 y_pred = np.zeros(20)
 print(misclassification_error(y_true, y_pred))
 print(misclassification_error(y_true, y_pred, normalize=False))
+print(accuracy(y_true, y_pred))
