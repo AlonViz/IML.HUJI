109018895e769e71155d90f4da82e736e8b39957
alonv <alonvizner1@gmail.com>
2022-04-28 00:22:37 +0300

    add testing of ensemble methods

---
+++ Ex3/gaussian_naive_bayes.py
@@ -0,0 +1,109 @@
+from typing import NoReturn
+from ...base import BaseEstimator
+import numpy as np
+
+
+class GaussianNaiveBayes(BaseEstimator):
+    """
+    Gaussian Naive-Bayes classifier
+    """
+
+    def __init__(self):
+        """
+        Instantiate a Gaussian Naive Bayes classifier
+
+        Attributes
+        ----------
+        self.classes_ : np.ndarray of shape (n_classes,)
+            The different labels classes. To be set in `GaussianNaiveBayes.fit`
+
+        self.mu_ : np.ndarray of shape (n_classes,n_features)
+            The estimated features means for each class. To be set in `GaussianNaiveBayes.fit`
+
+        self.vars_ : np.ndarray of shape (n_classes, n_features)
+            The estimated features variances for each class. To be set in `GaussianNaiveBayes.fit`
+
+        self.pi_: np.ndarray of shape (n_classes)
+            The estimated class probabilities. To be set in `GaussianNaiveBayes.fit`
+        """
+        super().__init__()
+        self.classes_, self.mu_, self.vars_, self.pi_ = None, None, None, None
+
+    def _fit(self, X: np.ndarray, y: np.ndarray) -> NoReturn:
+        """
+        fits a gaussian naive bayes model
+
+        Parameters
+        ----------
+        X : ndarray of shape (n_samples, n_features)
+            Input data to fit an estimator for
+
+        y : ndarray of shape (n_samples, )
+            Responses of input data to fit to
+        """
+        self.classes_, nk = np.unique(y, return_counts=True)
+        A = np.stack([np.where(y == class_, 1, 0) for class_ in self.classes_])
+        # A[i][j] == 1 iff classes_[i] == y[j]. A: (n_classes, n_samples)
+        self.mu_ = (A @ X) / nk[:, None]
+        self.pi_ = nk / y.size
+        self.vars_ = np.stack([np.var(X[y == class_, :], ddof=1, axis=0) for class_ in self.classes_])
+
+    def _predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        Predict responses for given samples using fitted estimator
+
+        Parameters
+        ----------
+        X : ndarray of shape (n_samples, n_features)
+            Input data to predict responses for
+
+        Returns
+        -------
+        responses : ndarray of shape (n_samples, )
+            Predicted responses of given samples
+        """
+        return np.take(self.classes_, (np.argmax(self.likelihood(X), axis=1)))
+
+    def likelihood(self, X: np.ndarray) -> np.ndarray:
+        """
+        Calculate the likelihood of a given data over the estimated model
+
+        Parameters
+        ----------
+        X : np.ndarray of shape (n_samples, n_features)
+            Input data to calculate its likelihood over the different classes.
+
+        Returns
+        -------
+        likelihoods : np.ndarray of shape (n_samples, n_classes)
+            The likelihood for each sample under each of the classes
+
+        """
+        if not self.fitted_:
+            raise ValueError("Estimator must first be fitted before calling `likelihood` function")
+
+        # Expression derived in similar way to recitation, only 'cov' matrix is diagonal and differs between classes.
+        A = (1 / self.vars_) * self.mu_
+        B = np.log(self.pi_) - 0.5 * np.sum(np.square(self.mu_) * (1 / self.vars_), axis=1)
+        G = np.log(np.abs(np.prod(self.vars_, axis=1)))
+        return X @ np.transpose(A) + B[:, ] - 0.5 * (np.square(X) @ np.transpose((1 / self.vars_))) - 0.5 * G
+
+    def _loss(self, X: np.ndarray, y: np.ndarray) -> float:
+        """
+        Evaluate performance under misclassification loss function
+
+        Parameters
+        ----------
+        X : ndarray of shape (n_samples, n_features)
+            Test samples
+
+        y : ndarray of shape (n_samples, )
+            True labels of test samples
+
+        Returns
+        -------
+        loss : float
+            Performance under missclassification loss function
+        """
+        from ...metrics import misclassification_error
+        return misclassification_error(self.predict(X), y)
